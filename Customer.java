d9f3c46227f4190a563e425c831136bc07d83e23
EricCortez <65201469+EricCortez@users.noreply.github.com>
2021-07-05 16:49:10 -0500

    Add files via upload
    Constructor empty parameters

---
+++ Customer.java
@@ -0,0 +1,24 @@
+public class Customer {
+    private int id;
+    private  String name;
+    private  String dob;
+    private  double salary;
+    private String email;
+    private  int age;
+
+
+
+    public Customer(int id, String name, String dob, double salary, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.salary = salary;
+        this.email = email;
+        this.age = age;
+    }
+
+
+    public Customer() {
+
+    }
+}
